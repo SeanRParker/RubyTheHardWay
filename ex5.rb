a19aa16d06c857820b40f20d4339408da9a60cce
Shaws <seanrichardparker@gmail.com>
2017-03-08 07:54:36 -0500

    simplifying var names 'my_' was redundant.

--- ex5.rb
+++ ex5.rb
@@ -1,17 +1,17 @@
-my_name = 'Sean R. Parker'
-my_age = 29 #as of 03/2017
-my_height = (5 * 12) + 8 # inches
-my_weight = 167 # lbs
-my_eyes = 'Brown'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Sean R. Parker'
+age = 29 #as of 03/2017
+height = (5 * 12) + 8 # inches
+weight = 167 # lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth}."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth}."
 
 # this line is tricky try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight}, I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight}, I get #{age + height + weight}."
