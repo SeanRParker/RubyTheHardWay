4b2aeb1532e74ef43ab3a06f5b3e6061ba7a6397
Shaws <seanrichardparker@gmail.com>
2017-03-07 22:14:52 -0500

    comment coding the vars

--- ex4.rb
+++ ex4.rb
@@ -1,11 +1,20 @@
-cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
-cars_not_driven = cars - drivers
-cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
-average_passengers_per_car = passengers / cars_driven
+# Assigning variables
+  # 100 cars
+  cars = 100
+  # 4 seats per car
+  space_in_a_car = 4.0
+  # 30 drivers today
+  drivers = 30
+  # 90 passengers today
+  passengers = 90
+  # leftover cars
+  cars_not_driven = cars - drivers
+  # all the cars need drivers but we only have 30
+  cars_driven = drivers
+  # calculate total seats available
+  carpool_capacity = cars_driven * space_in_a_car
+  # taken seats per car
+  average_passengers_per_car = passengers / cars_driven
 
 puts "There are #{cars} cars available."
 puts "There are only #{drivers} drivers available."
