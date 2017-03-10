d7c5591a1396989390effd759e2ebdb604b62356
Shaws <seanrichardparker@gmail.com>
2017-03-09 21:56:22 -0500

    examples calling arguments with commands

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# Call arguments in terminal like so:
+# ruby ex13.rb first 2nd 3 five
