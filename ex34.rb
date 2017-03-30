827df9c94e69956013882b8b3a2bc7e9512a661b
Shaws <seanrichardparker@gmail.com>
2017-03-29 21:49:42 -0400

    array item output practice

---
+++ ex34.rb
@@ -0,0 +1,12 @@
+animals = ['bear', 'ruby', 'cardinal', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts "animals = #{animals.to_s}"
+
+puts "The animal at 1 is a #{animals[0]}"
+puts "The third (3rd) animal is a #{animals[2]}"
+puts "The first (1st) animal is a #{animals[0]}"
+puts "The animal at 3 (#{animals[2]})"
+puts "The fifth (5th) animal is a #{animals[4]}"
+puts "The animal at 2 (#{animals[1]})"
+puts "The sixth (6th) animal is a #{animals[5]}"
+puts "The animal at 4 (#{animals[3]})"
